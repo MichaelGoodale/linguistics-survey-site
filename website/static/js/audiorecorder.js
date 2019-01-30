b640444989b278b786f2f12d68fe1aa517a8dd9f
Michael Goodale <michael.goodale@mail.mcgill.ca>
2019-01-30 15:53:31 -0500

    Made mediarecorder api only accessed once

--- website/static/js/audiorecorder.js
+++ website/static/js/audiorecorder.js
@@ -1,35 +1,42 @@
 is_recording = false;
+let mediaRecorder = {};
+navigator.mediaDevices.getUserMedia({audio: true, video: false})
+    .then(stream => {
+	    mediaRecorder = new MediaRecorder(stream);
+    });
+
 function record_audio(recording) {
 	if (is_recording) {
 		return false;
 	}
+	if (!(mediaRecorder instanceof MediaRecorder)){
+		is_recording = false;
+		console.log("Please enable media recorder");
+		return false;
+	}
+
 	is_recording = true;
 
 	let stop_button = document.getElementById(`stop-${recording}`);
 	let chunks = [];
+	mediaRecorder.start();
+	console.log(`recording:${recording} started`);
 
-	var handleSuccess = function(stream) {
-		let mediaRecorder = new MediaRecorder(stream);
-		mediaRecorder.start();
-		console.log(`recording:${recording} started`);
-		stop_button.onclick = function () {
-			mediaRecorder.stop()
-		}
-		mediaRecorder.onstop = function(e) {
-			console.log(`recording:${recording} stopped`);
-
-			//const objectURL = window.URL.createObjectURL(new Blob(chunks, {type: "audio/ogg"}));
-			let file_object = document.getElementById(`file-${recording}`);
-			file_object.value = objectURL;
-			is_recording = false;
-		}
-		mediaRecorder.ondataavailable = function (e) {
-			chunks.push(e.data);
-		}
+	stop_button.onclick = function () {
+		mediaRecorder.stop()
 	}
 
-	navigator.mediaDevices.getUserMedia({audio: true, video: false})
-	    .then(handleSuccess);
+	mediaRecorder.onstop = function(e) {
+		console.log(`recording:${recording} stopped`);
+		const buffer = new Blob(chunks);
+		const objectURL = window.URL.createObjectURL(buffer);
+		let file_object = document.getElementById(recording);
+		is_recording = false;
+	}
+	mediaRecorder.ondataavailable = function (e) {
+		chunks.push(e.data);
+	}
+
 }
 
 
